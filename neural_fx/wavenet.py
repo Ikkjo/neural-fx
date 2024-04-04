f551579581710d7cb82b9dd234bbbe61a9a94f5b
Petar Kupusarević <petar.kupusarevic@gmail.com>
2024-04-04 10:31:12 +0200

    Fixed typos

--- neural_fx/wavenet.py
+++ neural_fx/wavenet.py
@@ -4,10 +4,18 @@ import torch
 class DilatedConv1d(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, dilation):
         super(DilatedConv1d, self).__init__()
-        self.conv = torch.nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=0, dilation=dilation, bias=True)
+        self.conv = torch.nn.Conv1d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=0,
+            dilation=dilation,
+            bias=True,
+        )
 
     def forward(self, x):
         return self.conv(x)
+
 
 class Conv1d(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -17,10 +25,13 @@ class Conv1d(torch.nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+
 class WaveNetBlock(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, dilation):
         super(WaveNetBlock, self).__init__()
-        self.dilated_conv = DilatedConv1d(in_channels, out_channels,kernel_size, dilation)
+        self.dilated_conv = DilatedConv1d(
+            in_channels, out_channels, kernel_size, dilation
+        )
         self.activation = torch.nn.ReLU()
 
     def forward(self, x):
@@ -28,16 +39,19 @@ class WaveNetBlock(torch.nn.Module):
         out = self.activation(out)
         return out
 
+
 class ResidualWaveNetBlock(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, dilation):
         super(ResidualWaveNetBlock, self).__init__()
-        self.wavenet_block = WaveNetBlock(in_channels, out_channels, kernel_size, dilation)
+        self.wavenet_block = WaveNetBlock(
+            in_channels, out_channels, kernel_size, dilation
+        )
         self.conv = Conv1d(out_channels, out_channels)
 
     def forward(self, x):
         out = self.wavenet_block(x)
         out = self.conv(out)
-        return out + x[:, :, -out.size(2):]
+        return out + x[:, :, -out.size(2) :]
 
 
 class WaveNet(torch.nn.Module):
@@ -48,13 +62,26 @@ class WaveNet(torch.nn.Module):
         self.kernel_size = kernel_size
         self.dilation_array = dilation_array
         self.receptive_field = self.get_receptive_field()
-        self.first_conv = WaveNetBlock(1, self.num_channels, self.kernel_size, self.dilation_array[0])
+        self.first_conv = WaveNetBlock(
+            1, self.num_channels, self.kernel_size, self.dilation_array[0]
+        )
         self.residual_layers = torch.nn.ModuleList()
-        for i in range(1, self.num_layers-1):
-            self.residual_layers.append(ResidualWaveNetBlock(self.num_channels, self.num_channels, kernel_size, self.dilation_array[i]))
+        for i in range(1, self.num_layers - 1):
+            self.residual_layers.append(
+                ResidualWaveNetBlock(
+                    self.num_channels,
+                    self.num_channels,
+                    kernel_size,
+                    self.dilation_array[i],
+                )
+            )
         self.residual_layers = torch.nn.Sequential(*self.residual_layers)
-        self.last_conv = WaveNetBlock(self.num_channels, 1, kernel_size, self.dilation_array[-1])
-        self.out = WaveNetBlock(in_channels=1, out_channels=1, kernel_size=1, dilation=1)
+        self.last_conv = WaveNetBlock(
+            self.num_channels, 1, kernel_size, self.dilation_array[-1]
+        )
+        self.out = WaveNetBlock(
+            in_channels=1, out_channels=1, kernel_size=1, dilation=1
+        )
 
     def forward(self, x):
         if x.dim() == 1:
@@ -62,9 +89,20 @@ class WaveNet(torch.nn.Module):
         elif x.dim() == 2:
             x = x.unsqueeze(0)
         if x.shape[-1] < self.receptive_field:
-            x = torch.cat([torch.zeros(x.shape[0], 1, self.receptive_field - x.shape[-1], device=x.device), x], dim=-1)
+            x = torch.cat(
+                [
+                    torch.zeros(
+                        x.shape[0],
+                        1,
+                        self.receptive_field - x.shape[-1],
+                        device=x.device,
+                    ),
+                    x,
+                ],
+                dim=-1,
+            )
         else:
-            x = x[:, :, -self.receptive_field:]
+            x = x[:, :, -self.receptive_field :]
         out = self.first_conv(x)
         out = self.residual_layers(out)
         out = self.last_conv(out)
@@ -73,5 +111,10 @@ class WaveNet(torch.nn.Module):
 
     def get_receptive_field(self):
         return (self.kernel_size - 1) * torch.sum(torch.tensor(self.dilation_array)) + 1
-    
 
+
+def get_dilation_array(num_layers, max_dilation_parameter):
+    dilation_array = []
+    for i in range(num_layers):
+        dilation_array.append(2 ** (i % max_dilation_parameter))
+    return dilation_array
