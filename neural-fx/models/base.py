424cc753dc7f33256793d8e81fbfbf9a30eaa3ab
Ikkjo <ilija.kalinic@gmail.com>
2026-01-05 17:40:18 +0100

    Remove unused import

--- neural-fx/models/base.py
+++ neural-fx/models/base.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any, Optional, Union, Tuple, List
 from pathlib import Path
-import torch
 import torch.nn as nn
 from torch import Tensor
 
