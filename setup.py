3b44914bbf890f69516409f480c7b1c3de9b04e6
Ikkjo <ilija.kalinic@gmail.com>
2026-02-07 16:53:03 +0100

    Fix gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neural-fx',
+    version='0.1',
+    python_requires='>=3.10',
+    packages=find_packages(where="."),
+    package_dir={'neuralfx': 'neural_fx'},
+    include_package_data=True,
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "numpy",
+        "scipy",
+        "tqdm",
+        "matplotlib",
+        "lightning",
+        "pyyaml"
+    ],
+)
